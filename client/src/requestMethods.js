dfefeb3f3902f1919d208ba738331b019a4ca1bf
Subihi <SAbudurexiti@totalwine.com>
2022-06-12 08:52:15 -0400

    ssf

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = '/api';
+const BASE_URL = 'api';
 
 const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
