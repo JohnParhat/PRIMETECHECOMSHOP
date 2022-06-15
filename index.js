4bd15f191a37527929fb5a8544ed6111b1675ec0
JohnParhat <john.parhat@gmail.com>
2022-06-15 12:59:48 -0400

    sfsds

--- index.js
+++ index.js
@@ -10,22 +10,19 @@ const productRoute = require('./api/routes/product');
 const cartRoute = require('./api/routes/cart');
 const orderRoute = require('./api/routes/order');
 const stripeRoute = require('./api/routes/stripe');
-const path = require("path");
+const path = require('path');
 dotenv.config();
 app.use(cors());
 mongoose
-  .connect(
-    process.env.MONGODB_URL,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('DB connection successful'))
   .catch((err) => {
     console.log(err);
   });
 
-  
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
@@ -34,15 +31,15 @@ app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
 
-
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','index.html'));
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
+  console.log(PORT);
   console.log('Backend server is running!');
 });
